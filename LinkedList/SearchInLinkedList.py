2f85a0524002bbefe7c04bc7f39f23035980ec0a
Sunil <sunil016@yahoo.com>
2020-04-19 12:55:07 +0530

    Linked List Search and Display Class Methods

--- LinkedList/SearchInLinkedList.py
+++ LinkedList/SearchInLinkedList.py
@@ -4,23 +4,40 @@ class Node:
         self.next = None
 
 
-listOne = Node("One")
-listOne.next = Node("Two")
-listOne.next.next = Node("Three")
-listOne.next.next.next = Node("Four")
-listOne.next.next.next.next = Node("Five")
+class SingleLinkedList:  # Single Linked List Implementation
+    def __init__(self):
+        self.head = None
+
+    def print_element_in_linked_list(self, key):
+        head = self.head
+        while head is not None:
+            if head.data == key:
+                return True
+            head = head.next
+        return False
+
+    def display_elements_in_linked_list(self):
+        head = self.head
+        while head is not None:
+            print(head.data+" "),  # Printing InLine
+            head = head.next
+
+
+_nodeA = Node("One")
+_nodeB = Node("Two")
+_nodeC = Node("Three")
+_nodeD = Node("Four")
+_nodeE = Node("Five")
+
+_list = SingleLinkedList()
+_list.head = _nodeA
+_nodeA.next = _nodeB
+_nodeB.next = _nodeC
+_nodeC.next = _nodeD
+_nodeD.next = _nodeE
 
 # Printing all the Elements in the Linked List
-
-
-def search_element(_list, key):
-    while _list is not None:
-        if _list.data == key:
-            return True
-        _list = _list.next
-    return False
-
-
-print(search_element(listOne, "Three"))
-
+print("Element Found "+str(_list.print_element_in_linked_list("Four")))
+print("Displaying All the Elements in SingleLinkedList")
+_list.display_elements_in_linked_list()
 
